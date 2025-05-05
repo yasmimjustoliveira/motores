51d9cd21bdfb180774b0e89a86556b0bf5ca2115
yasmimjustoliveira <yasmim.barbosa@escolar.ifrn.edu.br>
2025-05-05 12:05:41 -0300

    3

---
+++ motores1M/Assets/Play.cs
@@ -0,0 +1,21 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Play : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(numero)
+        //numero = numero + 1;
+    
+if (Input.GetKey(KeyCode.A))
+    
+    gameObject.transform.position += new Vector3(velocidade,0,0)
+    
